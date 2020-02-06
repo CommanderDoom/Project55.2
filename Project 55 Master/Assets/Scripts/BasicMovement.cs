44bd914205e81d05a63c2086a0c7a473efc5f821
CommanderDoom <commander_doom@yahoo.com>
2020-02-06 02:23:30 -0800

    Initial Commit
    Yup, Just a commit

---
+++ Project 55 Master/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody mech;
+    public int speed = 5;
+    public int rotationSpeed = 100;
+    public float jumpforce = 50f;
+    public int jumpcooldown = 1000;
+    private Vector3 translationSpeed;
+    private Vector3 jumpvector;
+    private int functionaljumpcooldown = 0;
+   
+
+    //intializing variables
+    void Start()
+    {
+        mech = GetComponent<Rigidbody>();
+        jumpvector = new Vector3(0, jumpforce, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gathering inputs
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        //adjusting inputs
+        translationSpeed = new Vector3(0, 0, translation);
+        rotation *= Time.deltaTime;
+
+        //applying inputs
+        transform.Rotate(0, rotation, 0);
+        mech.MovePosition(transform.position + transform.TransformDirection(translationSpeed) * Time.fixedDeltaTime);
+
+        //jumping
+        if (Input.GetAxis("Jump") > 0)
+        {
+            if (functionaljumpcooldown == 0)
+            {
+                mech.AddForce(jumpvector, ForceMode.Impulse);
+                functionaljumpcooldown = jumpcooldown;
+            }
+        }
+        if (functionaljumpcooldown > 0) { functionaljumpcooldown--; }
+        Debug.Log(functionaljumpcooldown);
+
+    }
+}
+
---
+++ Project 55 Master/Assets/Scripts/TorsoRotation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorsoRotation : MonoBehaviour
+{
+    public float speedH = 1.5f;
+    float yaw = 0.0f;
+    private int look = 0;
+    private float lookcontrol;
+    private int currentyaw = 0;
+    private int yawlimit = 45;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookcontrol = Input.GetAxis("Mouse X");
+
+        if (0 < lookcontrol && currentyaw < yawlimit)
+        {
+            look = 1;
+           currentyaw++;
+        }
+        else if (0 > lookcontrol && currentyaw > -yawlimit)
+        {
+            look = -1;
+            currentyaw--;
+        }
+        else
+        {
+            look = 0;
+        }
+
+        yaw = speedH * look;
+        transform.eulerAngles += new Vector3(0.0f, yaw, 0.0f);
+    }
+}
